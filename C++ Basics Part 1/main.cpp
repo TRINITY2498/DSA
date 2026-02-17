ac815e451a6308037978ed65c737c708fd84601a
TRINITY2498 <kaustubhk033@gmail.com>
2026-02-17 15:09:54 +0530

    Initial commit - C++ basics setup

---
+++ C++ Basics Part 1/main.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+/*
+
+int main() {
+  
+  int a,b;
+  cin>> a>>b;
+  cout<<a<<endl;
+  cout<<b<<endl;
+  cout<<"Hello World";
+  return 0;
+}
+
+*/
