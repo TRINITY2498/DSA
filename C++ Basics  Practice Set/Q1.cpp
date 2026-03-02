fff075221239eab0a254254697cbe894ae1bfb03
TRINITY2498 <kaustubhk033@gmail.com>
2026-03-02 12:59:23 +0530

    CPP Basics Practice Set Added.

---
+++ C++ Basics  Practice Set/Q1.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class solution{
+public:
+    void printInput(){
+       
+       int a;
+       string s;
+       
+       cin >> a;
+       cin.ignore();
+       getline(cin,s);
+       
+       cout << a << " " << s;
+       
+    }
+};
---
+++ C++ Basics  Practice Set/Q2.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class solution{
+public:
+    string comparison(int n, int m){
+        
+        if (n < m){
+            
+            cout << "lesser" << endl;
+        }
+        
+        else if (n == m){
+            
+            cout << "equal" << endl;
+        }
+        
+        else{
+            
+            cout << "greater" << endl;
+        }
+        
+    }
+};
